b47f380ab6ed76806f8df180cfc6748f8e4d90ca
Roman Maru <romanmaru10@gmail.com>
2020-05-24 05:22:34 -0700

    Add form notification from contacy form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,18 @@
 import React from 'react'
 
 const Contact = () => (
-     <div className="contact">
-      <h3 className="contact_text">Get in touch</h3>
-      <form>
-        <fieldset>
-          <input type="text" placeholder="Your Name"/>
-          <input type="email" placeholder="Your Email"  />
-          <textarea placeholder="Your Message"/>
-          <input type="button" value="send"/>
-        </fieldset>
-      </form>
-    </div>
+  <div className="contact">
+    <h3 className="contact_text">Get in touch</h3>
+    <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+    <input type="hidden" name="bot-field" />
+      <fieldset>
+        <input type="text" placeholder="Your Name" />
+        <input type="email" placeholder="Your Email" />
+        <textarea placeholder="Your Message" />
+        <input type="submit" value="send" />
+      </fieldset>
+    </form>
+  </div>
 )
 
 export default Contact
