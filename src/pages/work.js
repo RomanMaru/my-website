c8f45cf3ec9653f645bab66ad5804f8e69d040e3
Roman Maru <romanmaru10@gmail.com>
2020-05-26 00:07:02 -0700

    copy code from demo contact form

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import {navigate} from 'gatsby-link'
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
-const Work = () => (
+const Work = () => {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+  return (
     <div className="work">
-        <form
-            name="test"
-            method="post"
-            data-netlify-honeypot="bot-field"
-            data-netlify="true">
-            <input type="hidden" name="form-name" value="test" />
-            <input type="text" name="word" />
-            <button role="submit">send</button>
-        </form>
+      <form
+        name="test"
+        method="post"
+        data-netlify-honeypot="bot-field"
+        data-netlify="true"
+        onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="test" />
+        <input type="text" name="word" onChange={handleChange}/>
+        <button role="submit">send</button>
+      </form>
     </div>
-)
+  )
+}
+
 
 export default Work
